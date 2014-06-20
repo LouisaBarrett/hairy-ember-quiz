2993ec2b31a705113e102f8c12753d24a4aaa013
Louisa Barrett <louisabarrett@gmail.com>
2014-06-20 14:30:56 -0600

    Refactor quizController to return total score of quiz

--- js/app.js
+++ js/app.js
@@ -9,117 +9,153 @@ App.Router.map(function() {
   this.resource("quiz");
 });
 
-App.WarmupController = Ember.View.extend({
-  'warmupTitle': "Warm Up Question",
-  'warmupQuestions': [{
-      question: 'Warm up questions are fun!',
-      warmupOptions: [{
-        display: 'True',
-        value: 0
-      }, {
-        display: 'False',
-        value: 1
-      }]
-    }],
+App.QuizController = Ember.ArrayController.extend({
+  'score': function() {
+    var total = 0;
+    this.get('questions').forEach(function(question) {
+      total += question.value;
+    });
+    return total;
+  }.property('@each.value'),
+  quizTitle: "The Actual Quiz",
+  questions: [{
+    id: 1,
+    question: "Fish talk a lot",
+    value: 0,
+    answerOptions: [{
+      display: "True",
+      value: 0
+    }, {
+      display: "False",
+      value: 1
+    }]
+  }, {
+    id: 2,
+    question: "Cats are not dogs",
+    value: 0,
+    answerOptions: [{
+      display: "True",
+      value: 1
+    }, {
+      display: "False",
+      value: 0
+    }]
+  }, {
+    id: 3,
+    question: "Birds are mammals",
+    value: 0,
+    answerOptions: [{
+      display: "True",
+      value: 1
+    }, {
+      display: "False",
+      value: 0
+    }]
+  }, {
+    id: 4,
+    question: "Unicorns are real",
+    value: 0,
+    answerOptions: [{
+      display: "True",
+      value: 1
+    }, {
+      display: "False",
+      value: 0
+    }]
+  }, {
+    id: 5,
+    question: "Dinosaurs are awesome",
+    value: 1,
+    answerOptions: [{
+      display: "True",
+      value: 1
+    }, {
+      display: "False",
+      value: 0
+    }]
+  }]
 });
 
-App.QuizController = Ember.Controller.extend({
-  'quizTitle': 'The Actual Quiz',
-  'quizQuestions': [{
-      question: 'Fish talk a lot',
-      answerOptions: [{
-        display: 'True',
-        value: 0
-      }, {
-        display: 'False',
-        value: 1
-      }]
+// App.QuizQuestionsController = Ember.ArrayController.extend({
+//   itemController: 'questions'
+// });
+
+// App.QuizQuestionsController = Ember.ObjectController.extend({
+//   lookupItemController: function( object ) {
+//   if (object.get('value')) {
+//       alert('value');
+//     } else {
+//       alert('things');
+//     }
+//   }
+// });
+
+App.QuizRoute = Ember.Route.extend({
+  // model: function() {
+  //   return quiz;
+  // }
+  // setupController: function(controller, quiz) {
+  //   controller.set('model', quiz.get('questions'));
+  // }
+});
+
+// App.QuizController = Ember.Controller.extend({
+// });
+
+// App.WarmupRoute = Ember.Route.extend({
+//   model: function() {
+//     return warmup;
+//   }
+// });
+
+
+App.WarmupController = Ember.View.extend({
+  warmupTitle: "Warm Up Question",
+  warmupQuestions: [{
+    id: 0,
+    question: "Warm up questions are fun!",
+    warmupOptions: [{
+      display: "True",
+      value: 0
     }, {
-      question: 'Cats are not dogs',
-      answerOptions: [{
-        display: 'True',
-        value: 1
-      }, {
-        display: 'False',
-        value: 0
-      }]
-    }, {
-      question: 'Birds are mammals',
-      answerOptions: [{
-        display: 'True',
-        value: 1
-      }, {
-        display: 'False',
-        value: 0
-      }]
-    }, {
-      question: 'Unicorns are real',
-      answerOptions: [{
-        display: 'True',
-        value: 1
-      }, {
-        display: 'False',
-        value: 0
-      }]
-    }, {
-      question: 'Dinosaurs are awesome',
-      answerOptions: [{
-        display: 'True',
-        value: 1
-      }, {
-        display: 'False',
-        value: 0
-      }]
+      display: "False",
+      value: 1
+    }]
   }],
 });
 
+
 App.ClickWarmUpStatus = Ember.View.extend({
   click: function(evt) {
-    $(".js-total").html($(".js-answer").val());
+    // $(".js-total").html($(".js-answer").val());
+    this.resource('warmup')
   }
 });
 
 App.ClickQuizScore = Ember.View.extend({
   click: function(evt) {
-    // var answer1 = $(".js-answer1").val();
-    // if (answer1 == 0) {
-    //   $(".js-answer1").css("background-color", "red");
-    // } else {
-    //   $(".js-answer1").css("background-color", "lightgreen");
-    // }
 
-    // var answer2 = $(".js-answer2").val();
-    // if (answer2 == 0) {
-    //   $(".js-answer2").css("background-color", "red");
-    // } else {
-    //   $(".js-answer2").css("background-color", "lightgreen");
-    // }
 
-    // var answer3 = $(".js-answer3").val();
-    // if (answer3 == 0) {
-    //   $(".js-answer3").css("background-color", "red");
-    // } else {
-    //   $(".js-answer3").css("background-color", "lightgreen");
-    // }
-    console.log($('.question').length)
-    $('.question').each(function(i, el) {
-      console.log(i, el)
-      var $id, $answer, question, answerTotals;
+    var $id, $answer, question, answerTotals;
 
-      $id = $(el).attr('answerOptions');
-      $answer = $(el).val();
-      question = questions[id];
+    // console.log($(".question").length);
+    // $(".question").each(function(i, el) {
+    //   // console.log(i, el);
+    //   // console.log("bananas!");
 
-      if ($answer == 0) {
-        $(el).css("background-color", "red");
-      } else {
-        $(el).css("background-color", "lightgreen");
-      }
+    //   $id = $(el).attr("answerOptions");
+    //   $answer = $(el).val();
+    //   question = $(el).attr("quizQuestions");
+    //   if ($answer == 0) {
+    //     $(el).css("background-color", "red");
+    //   } else {
+    //     $(el).css("background-color", "lightgreen");
+    //   }
 
-      answerTotals = Number(answer1) + Number(answer2) + Number(answer3);
-      $(".js-total2").html(answerTotals);
-    });
-
+    //   console.log($answer, "pants");
+    //   answerTotals = Number($answer);
+    //   $(".js-total2").html(answerTotals);
+    // });
   }
 
 });
