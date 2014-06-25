4a5ff4c04dd69484bf2542e49f57785381fe8966
Louisa Barrett <louisabarrett@gmail.com>
2014-06-24 22:00:25 -0600

    WIP: work on Get Score button returning user’s quiz score

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 App = Ember.Application.create({});
 
-App.Router.map(function() {
-  this.resource("quiz");
-});
-
 App.ApplicationController = Ember.Controller.extend({
   title: "Hairy Quiz, Ember Addition",
   description: "A very difficult and important quiz about Beasts"
+});
+
+App.Router.map(function() {
+  this.resource("quiz");
 });
 
 App.QuizRoute = Ember.Route.extend({
@@ -17,20 +17,26 @@ App.QuizRoute = Ember.Route.extend({
 });
 
 App.QuizController = Ember.ArrayController.extend({
+  'quizTitle': 'Beast Quiz'
+});
+
+App.ClickQuizScore = Ember.View.extend({
   actions: {
     'getScore': function() {
       alert('Score!')
         var total; 
         total = 0;
         this.get('questions').forEach(function(question) { 
-          if (question.answerOptions.selected == correctAnswer) {
+          if (question.answerOptions.display == correctAnswer) {
             total += 10;
           }
+          console.log(total);
         });
         return total;
       }.property('@each'),
     }
 });
+
 
 var questions = [{
   'id': 1,
